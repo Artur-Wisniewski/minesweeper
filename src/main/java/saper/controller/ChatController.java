9cbb2ea7a9ee260fa8751efbd2b22503f0fc3106
Artur Wiśniewski <artwis1998@gmail.com>
2020-01-08 19:16:36 +0100

    chat server

---
+++ src/main/java/saper/controller/ChatController.java
@@ -0,0 +1,44 @@
+package saper.controller;
+
+import static java.lang.String.format;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
+import saper.model.ChatMessage;
+
+@Controller
+public class ChatController {
+
+  private static final Logger logger = LoggerFactory.getLogger(WebSocketEventListener.class);
+
+  @Autowired
+  private SimpMessageSendingOperations messagingTemplate;
+
+  @MessageMapping("/chat/{roomId}/sendMessage")
+  public void sendMessage(@DestinationVariable String roomId, @Payload ChatMessage chatMessage) {
+    messagingTemplate.convertAndSend(format("/channel/%s", roomId), chatMessage);
+  }
+
+  @MessageMapping("/chat/{roomId}/addUser")
+  public void addUser(@DestinationVariable String roomId, @Payload ChatMessage chatMessage,
+      SimpMessageHeaderAccessor headerAccessor) {
+    String currentRoomId = (String) headerAccessor.getSessionAttributes().put("room_id", roomId);
+    //gdyby bylo cos tutaj to znaczyloby ze uzytkownik byl juz w jakims pokoju a to musimy obsluzyc
+    if (currentRoomId != null) {
+      ChatMessage leaveMessage = new ChatMessage();
+      leaveMessage.setType(ChatMessage.MessageType.LEAVE);
+      leaveMessage.setSender(chatMessage.getSender());
+      messagingTemplate.convertAndSend(format("/channel/%s", currentRoomId), leaveMessage);
+    }
+
+    headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+    messagingTemplate.convertAndSend(format("/channel/%s", roomId), chatMessage);
+  }
+}
