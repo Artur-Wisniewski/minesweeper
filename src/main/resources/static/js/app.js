ade6a5a37a3c68cdcaf2d73ee20ffa3bdf46bb9c
Artur Wiśniewski <artwis1998@gmail.com>
2020-01-08 19:03:55 +0100

    preventDefault

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,8 +1,15 @@
 
 
 function hide(){
+    if( $('#room-id').val() == "" ||  $('#name').val() == "" )
+    {
+        preventDefault();
+    }
     $("#stage1").hide();
     $("#stage2").attr("class","");
+
+    $("#room-id-display").text($("#room-id").val());
+
 }
 $(function () {
     $("form").on('submit', function (e) {
