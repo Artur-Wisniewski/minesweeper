b090675ee47f974f29975c2344f15894648c9ed7
Artur Wiśniewski <artwis1998@gmail.com>
2020-01-09 22:41:12 +0100

    working chat rooms

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,19 +1,124 @@
+var stompClient = null;
+var currentSubscription;
+var username = null;
+var roomId = null;
+var topic = null;
 
 
-function hide(){
+msgInput = $('#message');
+var colors = [
+    '#2196F3', '#32c787', '#00BCD4', '#ff5652',
+    '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
+];
+
+function connect(){
     if( $('#room-id').val() == "" ||  $('#name').val() == "" )
     {
         preventDefault();
     }
-    $("#stage1").hide();
-    $("#stage2").attr("class","");
+    username = $("#name").val().trim();
+    if (username) {
+        $("#stage1").hide();
+        $("#stage2").attr("class","");
 
-    $("#room-id-display").text($("#room-id").val());
 
+        var socket = new SockJS('/ws');
+        stompClient = Stomp.over(socket);
+
+        stompClient.connect({}, onConnected, onError);
+    }
+}
+function onConnected() {
+    enterRoom($("#room-id").val());
+    $(".connecting").hide();
+}
+
+function onError(error) {
+    $(".connecting").css("color","red");
+    $(".connecting").text('Could not connect to WebSocket server. Please refresh this page to try again!');
+}
+function enterRoom(newRoomId) {
+    roomId = newRoomId;
+    $("#room-id-display").text(roomId);
+    topic = `/app/chat/${newRoomId}`;
+
+    if (currentSubscription) {
+        currentSubscription.unsubscribe();
+    }
+    currentSubscription = stompClient.subscribe(`/channel/${roomId}`, onMessageReceived);
+
+    stompClient.send(`${topic}/addUser`,
+        {},
+        JSON.stringify({sender: username, type: 'JOIN'})
+    );
+}
+//do poprawienia wszyyyystko
+function onMessageReceived(payload) {
+    var message = JSON.parse(payload.body);
+
+    var messageElement = document.createElement('li');
+    if (message.type == 'JOIN') {
+        messageElement.classList.add('event-message');
+        message.content = message.sender + ' joined!';
+    } else if (message.type == 'LEAVE') {
+        messageElement.classList.add('event-message');
+        message.content = message.sender + ' left!';
+    } else {
+        messageElement.classList.add('chat-message');
+
+        var avatarElement = document.createElement('i');
+        var avatarText = document.createTextNode(message.sender[0]);
+        avatarElement.appendChild(avatarText);
+        avatarElement.style['background-color'] = getAvatarColor(message.sender);
+
+        messageElement.appendChild(avatarElement);
+
+        var usernameElement = document.createElement('span');
+        var usernameText = document.createTextNode(message.sender);
+        usernameElement.appendChild(usernameText);
+        messageElement.appendChild(usernameElement);
+    }
+
+    var textElement = document.createElement('p');
+    var messageText = document.createTextNode(message.content);
+    textElement.appendChild(messageText);
+
+    messageElement.appendChild(textElement);
+
+    $('#messageArea').append(messageElement);
+    $('#messageArea').scrollTop($('#messageArea').prop('scrollHeight'));
+}
+function sendMessage() {
+    var messageContent = $('#message').val().trim();
+    if (messageContent.startsWith('/join ')) {
+        var newRoomId = messageContent.substring('/join '.length);
+        enterRoom(newRoomId);
+        while ($('#messageArea:first-child')) {
+            $('#messageArea').remove($('#messageArea:first-child'));
+        }
+    } else if (messageContent && stompClient) {
+        var chatMessage = {
+            sender: username,
+            content: messageContent,
+            type: 'CHAT'
+        };
+        stompClient.send(`${topic}/sendMessage`, {}, JSON.stringify(chatMessage));
+    }
+    $('#message').val( '');
+}
+function getAvatarColor(messageSender) {
+    var hash = 0;
+    for (var i = 0; i < messageSender.length; i++) {
+        hash = 31 * hash + messageSender.charCodeAt(i);
+    }
+    var index = Math.abs(hash % colors.length);
+    return colors[index];
 }
 $(function () {
     $("form").on('submit', function (e) {
         e.preventDefault();
     });
-    $( "#join" ).click(function() { hide(); });
+    $( "#join" ).click(function() { connect(); });
+    $("#send").click(function () {  sendMessage()});
+
 });
